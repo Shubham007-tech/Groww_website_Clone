68e3da26339eb1869402b25d1291d357de6f6e9b
Shubham007-tech <91582453+Shubham007-tech@users.noreply.github.com>
2022-02-11 12:15:12 +0530

    cart script update

--- Front end/scripts/cart.js
+++ Front end/scripts/cart.js
@@ -33,7 +33,7 @@ function showProduct(data){
         
         btnFund.innerHTML = `Explore Funds`
         btnFund.onclick = () => {
-         window.location.href = `./Front end/groww_home.html`;   //./Front end/groww_home.html
+         window.location.href = `../Front end/groww_products.html`;   //./Front end/groww_home.html
         }  // http://127.0.0.1:5500/Front%20end/groww_products.html
 
         container.append(h1,img,btnFund);
